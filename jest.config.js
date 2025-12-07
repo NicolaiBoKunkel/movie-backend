7fd03646e8ec92bd50c1882bdc406958e61b85bd
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2025-12-07 14:57:22 +0100

    Unit test created by Test Cases

--- jest.config.js
+++ jest.config.js
@@ -11,8 +11,25 @@ module.exports = {
   coverageDirectory: 'coverage',
   verbose: true,
   testTimeout: 30000,
-  setupFilesAfterEnv: ['<rootDir>/Tests/Integration_Test/setup.js'],
-  globalTeardown: '<rootDir>/Tests/Integration_Test/teardown.js',
+  // Conditional setup based on test type
+  setupFilesAfterEnv: process.env.TEST_TYPE === 'unit' ? [] : ['<rootDir>/Tests/Integration_Test/setup.js'],
+  globalTeardown: process.env.TEST_TYPE === 'unit' ? undefined : '<rootDir>/Tests/Integration_Test/teardown.js',
   forceExit: true,
   detectOpenHandles: true,
+  // Test path patterns for different test types
+  projects: [
+    {
+      displayName: 'unit',
+      testMatch: ['<rootDir>/Tests/Unit_Test/**/*.test.js'],
+      testEnvironment: 'node',
+      setupFilesAfterEnv: [],
+    },
+    {
+      displayName: 'integration',
+      testMatch: ['<rootDir>/Tests/Integration_Test/**/*.test.js'],
+      testEnvironment: 'node',
+      setupFilesAfterEnv: ['<rootDir>/Tests/Integration_Test/setup.js'],
+      globalTeardown: '<rootDir>/Tests/Integration_Test/teardown.js',
+    }
+  ],
 };
