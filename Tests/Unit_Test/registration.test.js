4e06830f7c0a79e292482a814793aaab581428f6
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2025-12-16 12:17:57 +0100

    added value back about unit test breake

--- Tests/Unit_Test/registration.test.js
+++ Tests/Unit_Test/registration.test.js
@@ -85,7 +85,7 @@ describe('Registration Unit Tests', () => {
       const validUserData = {
         username: 'User01',
         email: 'user@example.com',
-        password: 'Secret12',
+        password: 'Secret123',
         confirmPassword: 'Secret123'
       };
 
