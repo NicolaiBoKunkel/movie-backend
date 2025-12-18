f9ab38ea7925a22171d9ba24a21fb2fbfdec055e
Jeffermus <70596979+Jeffermus@users.noreply.github.com>
2025-12-18 09:54:19 +0100

    brreake the pipe

--- src/routes/auth/schema.ts
+++ src/routes/auth/schema.ts
@@ -1,8 +1,8 @@
 import { z } from 'zod';
 
 export const registerSchema = z.object({
-  username: z.string().min(3).max(100), // unit break test (10)
-  password: z.string().min(6).max(200), // unit break test (10)
+  username: z.string().min(10).max(100), // unit break test (10)
+  password: z.string().min(10).max(200), // unit break test (10)
   role: z.enum(['admin','user']).optional()
 });
 
